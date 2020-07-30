0b5db3caeda077e6130aea262490f04bb8001c85
Rongxin Zhang <mrrongxin@gmail.com>
2020-07-29 21:53:47 -0700

    worsearch including stretch, without diagonal

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const {wordSearch, createDiagonalMatrix} = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,9 +15,25 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
+  });
+
+  it("should return false if the word is not present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'R', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'A', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'N', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'K', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK');
+
+    assert.isTrue(result);
   });
 
   it("should return true if the word is present", function() {
@@ -31,8 +47,73 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
+
+  it("should return false if empty array", function() {
+    const result = wordSearch([ ], 'BOB');
+    assert.isFalse(result);
+  });
+
+  it("should return true even if work is backwards", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['D', 'L', 'E', 'F', 'N', 'I', 'E', 'S'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  // it("should return diagonal arrays", function() {
+  //   const result = createDiagonalMatrix([
+  //     ["A","B","C","D"],
+  //     ["E","F","G","H"],
+  //     ["I","J","K","L"],
+  //     ["M","N","O","P"],
+  //   ], [
+  //     ["A","F","K","P"],
+  //     ["B","G","L"],
+  //     ["C","H"],
+  //     ["D"],
+  //     ["E", "J", "O"],
+  //     ["F", "K", "P"],
+  //     ["G", "L"],
+  //     ["H"],
+  //     ["I", "N"],
+  //     ["J", "O"],
+  //     ["K", "P"],
+  //     ["L"],
+  //     ["M"],
+  //     ["N"],
+  //     ["O"],
+  //     ["P"],
+  //   ]);
+
+  //   assert.isTrue(result);
+  // });
+
+  // it("should return true if word is diagonal", function() {
+  //   const result = wordSearch([
+  //     ['F', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+  //     ['S', 'R', 'I', 'C', 'F', 'E', 'L', 'D'],
+  //     ['Y', 'F', 'A', 'A', 'Q', 'U', 'A', 'L'],
+  //     ['H', 'M', 'J', 'N', 'E', 'V', 'R', 'G'],
+  //     ['W', 'H', 'C', 'H', 'K', 'E', 'R', 'L'],
+  //     ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+  //     ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+  //     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+  //     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  //   ], 'FRANK');
+
+  //   assert.isTrue(result);
+  // });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,82 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const createMatrix = function(col, row) {
+  let resMatrix = [];
+  for (let i = 0; i < row; i++) {
+    resMatrix.push(new Array(col).fill("x"));
+  }
+  return resMatrix;
+};
+  
+const transpose = function(matrix) {
+  let row = matrix.length;
+  let col = matrix[0].length;
+  
+  let newMatrix = createMatrix(row, col);
+  
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      newMatrix[j][i] = matrix[i][j];
     }
-}
+  }
+  return newMatrix;
+};
+  
+// const createDiagonalMatrix = function(matrix) {
+//   let responseMatrix = [];
+//   for (let i = 0; i < matrix.length; i++) {
+//     let diagonalWords = [];
+//     let counter = 0;
+//     for (let j = 0; j < matrix[i].length; j++) {
+//       // If first element
+//     //   if (j === 1) {
+//     //     diagonalWords.push(matrix[i][j]);
+//     //     matrix[i][j];
+//     //     // ["A","B","C","D"],
+//     //     // ["E","F","G","H"],
+//     //     // ["I","J","K","L"],
+//     //     // ["M","N","O","P"],
+//     //   } else {
+//       let diagRow = i + counter;
+//       let diagCol = j;
+        
+//       if (diagRow > matrix.length) {
+//         diagRow = i;
+//       }
+//       // if (diagCol > matrix[i].length) {
+//       //   diagCol = j;
+//       // }
+//       diagonalWords.push(matrix[diagRow][diagCol]);
+//       counter ++;
+//       //   }
+//       console.log(diagonalWords);
+//       responseMatrix.push(diagonalWords);
+//     }
+//   }
+// };
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+
+  if (letters.length <= 0) {
+    return false;
+  }
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  const reverseJoin = letters.map(ls => ls.reverse().join(''));
+  for (let l of reverseJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+// module.exports = {wordSearch, createDiagonalMatrix};
+module.exports = {wordSearch};
+
